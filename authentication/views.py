39b3fab5ca2bf870cff9e7390c9239b5aec1206b
jkaseq <egyabig2@gmail.com>
2023-03-28 21:59:15 +0300

    reviews 1

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.middleware import get_user
 from django.contrib.auth.models import update_last_login
+from django.http import HttpRequest, JsonResponse, HttpResponseServerError, HttpResponse
 from rest_framework import status, viewsets, permissions
 from rest_framework.decorators import action, api_view
 from rest_framework.response import Response
@@ -9,9 +11,9 @@ from rest_framework_simplejwt.tokens import Token
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics
 from authentication import utils
-from authentication.models import UserCreateCode, ActivityCategory
+from authentication.models import UserCreateCode, ActivityCategory, Review
 from authentication.serializers import UserCreateSerializer, MyTokenObtainPairSerializer, CodeSerializer, \
-    UpdatePasswordSerializer, ActivitySerializer
+    UpdatePasswordSerializer, ActivitySerializer, UserProfileSerializer, ReviewSerializer
 
 User = get_user_model()
 
@@ -38,7 +40,8 @@ class UserViewSet(viewsets.ModelViewSet, TokenObtainPairSerializer):
     def get_serializer_class(self):
         if self.action == 'update':
             return CodeSerializer
-        return UserCreateSerializer
+        # TODO поменять
+        return UserProfileSerializer
 
     def get_permissions(self):
         return super().get_permissions()
@@ -63,12 +66,64 @@ class UserViewSet(viewsets.ModelViewSet, TokenObtainPairSerializer):
 
     def perform_create(self, serializer):
         return serializer.save()
-
-    def retrieve(self, request, *args, **kwargs):
-        instance = request.user
-        serializer = self.get_serializer(instance)
-        return Response(serializer.data)
+    def retrieve(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        try:
+            user = get_user(request)
+            # TODO поменять
+            serializer = UserProfileSerializer(user)
+            print(type(serializer.data))
+            # serializer = self.get_serializer(instance)
+            return JsonResponse(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(f'Something goes wrong: {ex}')
 
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    def get_queryset(self):
+        return Review.objects.all()
+
+    def _create_review(self, request, message, mark, **extra_fields):
+        print(request)
+        print(get_user(request))
+        # groups = extra_fields.pop('groups', [])
+        # activities = extra_fields.pop('activities', [])
+        # ReviewModel = apps.get_model(self.model._meta.app_label, self.model._meta.object_name)
+        review = self.model(message=message, mark=mark, **extra_fields)
+        review.save(using=self._db)
+        # review.groups.set(groups)
+        # review.activities.set(activities)
+        return review
+
+    def create_review(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        review = self.perform_create(serializer)
+        print(serializer.data)
+        # headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def get_review(self, request: HttpRequest) -> HttpResponse:
+        try:
+            user = get_user(request)
+            # print(user.reviews)
+            serializer = ReviewSerializer(user.reviews, many=True)
+            # print(serializer.data)
+            # return JsonResponse(serializer.data, safe=False)
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(f'Something goes wrong: {ex}')
+
+    # def get_review(self, request: HttpRequest) -> HttpResponse:
+    #     try:
+    #         user = get_user(request)
+    #         serializer = self.serializer_class(data=request.data)
+    #         serializer.is_valid()
+    #         print(serializer.validated_data)
+    #         return HttpResponse(self.queryset.filter(user=user).values())
+    #     except Exception as ex:
+    #         return HttpResponseServerError(f'Something goes wrong: {ex}')
